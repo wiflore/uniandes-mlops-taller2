f0c8153c6b5e03eb7ff6ad94a3852aad5b374465
wiflore <williamflorezmaestre@gmail.com>
2026-01-27 23:22:25 -0500

    Implementar función load_data() para cargar datos de energía

--- app.py
+++ app.py
@@ -21,7 +21,10 @@ app.config.suppress_callback_exceptions = True
 
 # Load data from csv
 def load_data():
-    # To do: Completar la función 
+    df = pd.read_csv('datos_energia.csv')
+    df['time'] = pd.to_datetime(df['time'])
+    df = df.set_index('time')
+    return df
     
 
 # Cargar datos
